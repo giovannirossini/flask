4bc10799f6d8071aa5ff094c6cbced5db2aed1df
Giovanni Rossini <giovannijrrossini@gmail.com>
2022-09-25 00:44:40 -0300

    Deploy para Heroku

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 import os
-from app.main.base_service import service
+from app.app import app
 
 env = os.environ.get('FLASK_ENV')
 host = os.environ.get('HOST')
 port = os.environ.get('PORT')
 
 if env == "development":
-  service.run(host=host,port=port,debug=True)
+  app.run(host=host,port=port,debug=True)
 else:
-  service.run(host=host,port=port)
+  app.run(host=host,port=port)
